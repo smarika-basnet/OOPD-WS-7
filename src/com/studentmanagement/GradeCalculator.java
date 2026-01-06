69a90370f32541d3d339d5fabb319c69d51c0abc
Smarika Basnet <smarikabasnet059@gmail.com>
2026-01-06 08:45:19 +0545

    Initial commit

---
+++ src/com/studentmanagement/GradeCalculator.java
@@ -0,0 +1,11 @@
+package com.studentmanagement;
+
+public class GradeCalculator {
+    public double calculateAverage(int[] grades) {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.length;
+    }
+}
